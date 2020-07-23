5e5927d74f39b91fee7e092068236b37445aca87
Darkwolfxj <hesitation.dotdot@gmil.com>
2020-07-23 15:14:09 -0400

    finished day 4

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,10 +8,10 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
-        self.reg = [0] * 8
+        self.reg = [0] * 7 + [255]
         self.pc = 0
         self.isrunning = True
-        self.sp = 255
+        
 
     def load(self, p):
         """Load a program into memory."""
@@ -44,6 +44,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MULT":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -76,32 +78,46 @@ class CPU:
                 reg_address = self.ram_read(self.pc + 1)
                 reg_value = self.ram_read(self.pc + 2)
                 self.reg[reg_address] = reg_value
-                self.pc += 3
+            elif instruction == 0b10100000: # ADD
+                regA = self.ram_read(self.pc + 1)
+                regB = self.ram_read(self.pc + 2)
+                op = 'ADD'
+                self.alu(op, regA, regB)
             elif instruction == 0b10100010: # MULT
                 # store values in regA and regB
                 regA = self.ram_read(self.pc + 1)
                 regB = self.ram_read(self.pc + 2)
+                op = 'MULT'
                 # store product in regA
-                self.reg[regA] = self.reg[regA] * self.reg[regB]
-                self.pc += 3
+                self.alu(op, regA, regB)
             elif instruction == 0b01000101: # PUSH
-                self.sp -= 1
+                self.reg[7] -= 1
                 mdr = self.reg[self.ram_read(self.pc + 1)]
-                self.ram_write(self.sp, mdr)
-                self.pc += 2
+                self.ram_write(self.reg[7], mdr)
             elif instruction == 0b01000110: # POP 
                 regA = self.ram_read(self.pc + 1)
-                mdr = self.ram_read(self.sp)
+                mdr = self.ram_read(self.reg[7])
                 self.reg[regA] = mdr
-                self.pc += 2
-                self.sp += 1
+                self.reg[7] += 1
             elif instruction == 0b01000111: # PRN
                 print(self.reg[self.ram_read(self.pc + 1)])
-                self.pc += 2
             elif instruction == 0b00000001: # HLT
                 self.isrunning = False
+            elif instruction == 0b01010000: # CALL
+                jump_value = self.reg[self.ram_read(self.pc + 1)]
+                return_value = self.pc + 2
+                self.reg[7] -= 1
+                self.ram_write(self.reg[7], return_value) 
+                self.pc = jump_value
+                continue
+            elif instruction == 0b00010001: # RET
+                return_value = self.ram_read(self.reg[7])
+                self.reg[7] += 1
+                self.pc = return_value
+                continue
             else:
-                self.pc += 1
+                self.pc += 1 + (instruction >> 6)
+            self.pc += 1 + (instruction >> 6)
             if self.pc > 255:
                 self.isrunning = False
         
@@ -110,7 +126,3 @@ class CPU:
     
     def ram_write(self, mar, mdr):
         self.ram[mar] = mdr
-
-cpu = CPU()
-cpu.load(None)
-cpu.run()
